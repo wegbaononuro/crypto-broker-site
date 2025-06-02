189d254cfe44d9b307c28ebc0792c241c4190670
wegbaononuro <wegbaononuro@gmail.com>
2025-06-02 06:51:05 +0100

    Added Tailwind CSS styles and import

---
+++ public/code .ts
@@ -0,0 +1,17 @@
+code .
+export default {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+crypto-broker-site/src/index.css
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
